46d52918391c6b4c0bf007d2cee7f619e9a77f6c
Romain Marecat <romain.marecat@gmail.com>
2020-01-17 09:34:06 +0100

    Fix mock online session

--- src/lib/shared/session/mock-online-session.ts
+++ src/lib/shared/session/mock-online-session.ts
@@ -1,21 +1,16 @@
 import { OnlineSession } from './online-session';
 
 export const mockOnlineSession: OnlineSession = {
-  key: 'test1',
-  detail: {
-    name: 'test1',
-    max_persons: 1,
-    booking_delay: 1,
-    duration: 60,
-    pause: 0,
-  },
-  prices: [10, 20],
-  date_range: {
-    start: '2018-01-01',
-    end: '2019-12-31',
-  },
-  time_range: {
-    start: '08:00',
-    end: '19:00',
-  }
+  id: 'test1',
+  name: 'test1',
+  comment: 'test1',
+  max_persons: 1,
+  booking_delay: 1,
+  duration: 60,
+  pause: 0,
+  price: 10,
+  start_date: '2018-01-01',
+  end_date: '2019-12-31',
+  start_time: '08:00',
+  end_time: '19:00',
 };
